37f2b219be7705bcccccff3e636363dd1926b5ef
Byungjin Choi <chrisholic22@gmail.com>
2025-04-19 08:29:05 +0900

    Initial Commit

---
+++ onthelist/onthelistApp.swift
@@ -0,0 +1,17 @@
+//
+//  onthelistApp.swift
+//  onthelist
+//
+//  Created by 최병진 on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct onthelistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ onthelistTests/onthelistTests.swift
@@ -0,0 +1,17 @@
+//
+//  onthelistTests.swift
+//  onthelistTests
+//
+//  Created by 최병진 on 4/19/25.
+//
+
+import Testing
+@testable import onthelist
+
+struct onthelistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
